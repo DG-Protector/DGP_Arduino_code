a3a97ff3b8ddf18f8437340e9eae853cb3f65e84
Jihoon Kim (Hanul) <jihoonkimtech@naver.com>
2025-03-15 18:31:53 +0900

    Upload: BT tester

---
+++ ESP32_Blutooth/test_BT_ESP32/test_BT_ESP32.ino
@@ -0,0 +1,42 @@
+// test code for BT Serial
+
+/*
+ * bluetooth serial tester for ESP32
+ *
+ * written by jihoonkimtech
+ *
+ * last modified at 2025-02-23
+*/
+
+#include "BluetoothSerial.h"
+
+String device_name = "DG_Protector";
+
+// Check if Bluetooth is available
+#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
+#error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
+#endif
+
+// Check Serial Port Profile
+#if !defined(CONFIG_BT_SPP_ENABLED)
+#error Serial Port Profile for Bluetooth is not available or not enabled. It is only available for the ESP32 chip.
+#endif
+
+BluetoothSerial SerialBT;
+
+void setup() {
+  Serial.begin(115200);
+  SerialBT.begin(device_name);  //Bluetooth device name
+  //SerialBT.deleteAllBondedDevices(); // Uncomment this to delete paired devices; Must be called after begin
+  Serial.printf("\"%s\" is started.\n", device_name.c_str());
+}
+
+void loop() {
+  if (Serial.available()) {
+    SerialBT.write(Serial.read());
+  }
+  if (SerialBT.available()) {
+    Serial.write(SerialBT.read());
+  }
+  delay(20);
+}
