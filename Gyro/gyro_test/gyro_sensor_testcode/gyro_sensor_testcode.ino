c2f612f43e41fd0e2964788df084acbceb095003
krin3434 <yty2337@naver.com>
2025-03-20 22:25:02 +0900

    시리얼 모니터 수정

--- Gyro/gyro_test/gyro_sensor_testcode/gyro_sensor_testcode.ino
+++ Gyro/gyro_test/gyro_sensor_testcode/gyro_sensor_testcode.ino
@@ -87,22 +87,14 @@ void loop() {
 
   /*Print the obtained data on the defined format*/
   #ifdef OUTPUT_READABLE_ACCELGYRO
-    Serial.print("a/g:\t");
     Serial.print(ax); Serial.print("\t");
-    Serial.print(ay); Serial.print("\t");
-    Serial.print(az); Serial.print("\t");
-    Serial.print(gx); Serial.print("\t");
-    Serial.print(gy); Serial.print("\t");
-    Serial.println(gz);
+    Serial.print(ay); Serial.println("\t");
+
   #endif
 
   #ifdef OUTPUT_BINARY_ACCELGYRO
     Serial.write((uint8_t)(ax >> 8)); Serial.write((uint8_t)(ax & 0xFF));
     Serial.write((uint8_t)(ay >> 8)); Serial.write((uint8_t)(ay & 0xFF));
-    Serial.write((uint8_t)(az >> 8)); Serial.write((uint8_t)(az & 0xFF));
-    Serial.write((uint8_t)(gx >> 8)); Serial.write((uint8_t)(gx & 0xFF));
-    Serial.write((uint8_t)(gy >> 8)); Serial.write((uint8_t)(gy & 0xFF));
-    Serial.write((uint8_t)(gz >> 8)); Serial.write((uint8_t)(gz & 0xFF));
   #endif
 
   /*Blink LED to indicate activity*/
