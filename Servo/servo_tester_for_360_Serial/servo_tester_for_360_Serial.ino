1a210a3159003ae95d2cedb1fe1867c660c1355b
Jihoon Kim (Hanul) <jihoonkimtech@naver.com>
2025-03-26 23:39:44 +0900

    Fix: add serial buffer clear

--- Servo/servo_tester_for_360_Serial/servo_tester_for_360_Serial.ino
+++ Servo/servo_tester_for_360_Serial/servo_tester_for_360_Serial.ino
@@ -12,18 +12,18 @@ Servo R_SERVO;          // object of Servo class
 
 #define L_SERVO_PIN 10  // servo motor
 #define R_SERVO_PIN 11  // servo motor
-#define BKWD_BUT 3    // backward button
-#define FRWD_BUT 2    // forward button
 
-#define SERVO_FRWD 100  // rotate clockwise servo
-#define SERVO_BKWD 80   // rotate anti-clockwise servo
-#define SERVO_STOP 90   // stop rotate servo
+// L
+
+const byte R_SERVO_FRWD = 180;  // rotate clockwise servo
+const byte R_SERVO_BKWD = 0;   // rotate anti-clockwise servo
+const byte L_SERVO_FRWD = R_SERVO_FRWD;  // rotate clockwise servo
+const byte L_SERVO_BKWD = R_SERVO_BKWD;   // rotate anti-clockwise servo
+
+const byte SERVO_STOP = 90;   // stop rotate servo
 
 void setup() {
   Serial.begin(115200);       // for debug
-
-  pinMode(BKWD_BUT, INPUT_PULLUP);
-  pinMode(FRWD_BUT, INPUT_PULLUP);
 
   L_SERVO.attach(L_SERVO_PIN);  // attach servo
   R_SERVO.attach(R_SERVO_PIN);  // attach servo
@@ -33,33 +33,38 @@ void setup() {
 
 void loop() {
   if(Serial.available()){
-    int buf = Serial.parseInt();
+    char buf = Serial.read();
+    
     Serial.println(buf);
-    if(buf > 0){
-      L_SERVO.write(SERVO_BKWD);
-      R_SERVO.write(SERVO_FRWD);
+    if(buf == 'l'){
+      L_SERVO.write(L_SERVO_BKWD);
     }
-    else if(buf < 0){
-      L_SERVO.write(SERVO_FRWD);
-      R_SERVO.write(SERVO_BKWD);
+    else if(buf == 'L'){
+      L_SERVO.write(L_SERVO_FRWD);
+    }
+    else if(buf == 'r'){
+      R_SERVO.write(R_SERVO_FRWD);
+    }
+    else if(buf == 'R'){
+      R_SERVO.write(R_SERVO_BKWD);
+    }
+    else if(buf == '+'){
+      delay(1000);
+      L_SERVO.write(L_SERVO_BKWD);
+      R_SERVO.write(R_SERVO_FRWD);
+    }
+    else if(buf == '-'){
+      delay(1000);
+      L_SERVO.write(L_SERVO_FRWD);
+      R_SERVO.write(R_SERVO_BKWD);
     }
     else{
       L_SERVO.write(SERVO_STOP);  // stop servo
       R_SERVO.write(SERVO_STOP);  // stop servo
     }
-    buf = Serial.parseInt();
+    buf = Serial.read();
   }
   delay(50);
 
   
-}
-
-bool _checkButton(int pin){ // double check for chattering
-  if(!digitalRead(pin)){
-    delay(50);
-    if(!digitalRead(pin)) return true;
-    else return false;
-  } else {
-    return false;
-  }
 }
