ec33bd5180df7a743125351c1e3bcbb6898ba950
Jihoon Kim (Hanul) <jihoonkimtech@naver.com>
2025-03-29 22:01:21 +0900

    Fix: wrong class name

--- library/DGP_Fields.h
+++ library/DGP_Fields.h
@@ -3,6 +3,7 @@
 
 #include "DGP.h"
 
+
 /*
  * library of field dividing
  *
@@ -11,8 +12,26 @@
  * last modified at 2025-03-29
 */
 
-/* about fields
- * |op  |paramL  |separtor |paramR |eof  |
+class DGP_Fields {
+    private:
+        char op;                    // operator
+        uint8_t paramLeft;          // parameter of left servo
+        uint8_t paramRight;         // parameter of right servo
+
+    public:
+        DGP_Fields(){;}
+        ~DGP_Fields(){;}
+
+        char getOp(){ return op; };               // get operator
+        uint8_t getPL(){ return paramLeft; };    // get paramLeft
+        uint8_t getPR(){ return paramRight; };   // get paramRight
+
+        void extractField(String str);  // extract fields from string
+        void printSerialField();        // display fields
+};
+
+/* ################# about fields #######################
+ * |op  |paramL  |separator |paramR |eof  |
  * op is operator (it's char)
     - 'u' or 'U' : user setting.
     - 'l' or 'L' : low power setting for female(lowercase) or male(uppercase). no need param.
@@ -20,29 +39,18 @@
     - 'h' or 'H' : high power setting for female(lowercase) or male(uppercase). no need param.
  * paramL is parameter of left motor (it's short)
     - only values between 0 and 100
- * separtor is for distinguish between parameters (it's always (char)',')
+ * separator is for distinguish between parameters (it's always (char)',')
  * paramR is parameter of right motor (it's short)
     - only values between 0 and 100
  * eof is for point end of field (it's always (char)'.')
-*/
-class DGP_Fields {
-    public:
-        char op;                    // operator
-        uint8_t paramLeft = 0;      // parameter of left servo
-        uint8_t paramRight = 0;     // parameter of left servo
-        void extractField(String str);
-        void printSerialField();
-}
-
-void DPG_Fields::extractField(String str){  // extract fields from string
-    struct Fields f;
-
+########################################################## */
+void DGP_Fields::extractField(String str){
   // read op
   op = str.charAt(0);          // copy op field
-  str.remove(0,1);               // remove op in origin fields
+  str.remove(0,1);             // remove op in origin fields
 
     bool isNeedParams = false;
-    switch(op){   // check need param
+    switch(op){     // check need param
         case 'l':   // case of low power for female setting
         case 'L':   // case of low power for male setting
         case 'm':   // case of middle power for female setting
@@ -69,9 +77,9 @@ void DPG_Fields::extractField(String str){  // extract fields from string
         str.remove(0, ref+1);                           // remove paramR & end of field
     }
     return;
-}
+};
 
-void DPG_Fields::printSerialField(){      // display fields
+void DGP_Fields::printSerialField(){
     Serial.println("=====start=====");
     Serial.print("op : ");
     Serial.println(op);
@@ -80,7 +88,7 @@ void DPG_Fields::printSerialField(){      // display fields
     Serial.print("pR : ");
     Serial.println(paramRight);
     Serial.println("======end======");
-}
+};
 
 
 
