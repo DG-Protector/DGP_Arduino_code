23c551d43f92302ae566c2ebfec7b5066c95993c
Jihoon Kim (Hanul) <jihoonkimtech@naver.com>
2025-03-30 06:49:19 +0900

    Update: Implement all func

--- library/DGP_Gyro.h
+++ library/DGP_Gyro.h
@@ -48,4 +48,72 @@ DGP_Gyro::DGP_Gyro(uint8_t d, uint8_t c, float t = 0.95){
     thRef = t;
 }
 
+void DGP_Gyro::init(){
+    Wire.begin();       // wire communication start
+    gyro.initialize();  // gyro module init
+
+    delay(1000);        // connection wait
+    
+    return;
+}
+
+void DGP_Gyro::refresh(){                               // refresh value
+    int16_t stackX = 0;
+    int16_t stackY = 0;
+
+    for(uint8_t i = 0; i < CALC_SAMPLES; i++){
+        int16_t ax, ay, az;
+        int16_t gx, gy, gz;
+
+        gyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);  // read value
+
+        stackX += ax;
+        stackY += ay;                                   // stack value
+
+        delay(10);                                      //sample delay
+    }
+
+    calc_x = stackX/CALC_SAMPLES;
+    calc_y = stackY/CALC_SAMPLES;                 // calculate average
+    
+    return;
+}
+
+void DGP_Gyro::calibration(){                           // calibration and set cali_x, calc_y
+    int16_t stackX = 0;
+    int16_t stackY = 0;
+
+    for(uint8_t i = 0; i < CALI_SAMPLES; i++){
+        refresh();                                      // refresh calc_x, calc_y
+        stackX += calc_x;
+        stackY += calc_y;
+    }
+    cali_x = stackX/CALI_SAMPLES;
+    cali_y = stackY/CALI_SAMPLES;                       // make calibrate value
+
+    thhold_x = cali_x * thRef;
+    thhold_y = cali_y * thRef;                          // set threshold
+
+    isCompCali = true;
+
+    return;
+}
+
+void DGP_Gyro::disableCali(){
+    isCompCali = false;
+
+    return;
+}
+
+boolean DGP_Gyro::compareValue(){
+    refresh();
+    if (calc_x < thhold_x || calc_y < thhold_y) {   // good state
+        return true;
+    } 
+    else {
+        return false;
+    }
+}
+
+
 #endif
